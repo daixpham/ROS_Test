1269679ac1986cae1b7553a375e6c84fe498006e
estauseb <erik.stausebach@st.ovgu.de>
2018-10-22 17:14:22 +0200

    update

--- ros_test/src/turtleHouse.cpp
+++ ros_test/src/turtleHouse.cpp
@@ -1,15 +1,11 @@
 #include <ros_test/turtle_abstract.h>
 #include "ros/ros.h"
-#include <geometry_msgs/Twist.h>
 
 void drawHouse(int scale);
 void doWeirdStuff();
 int main(int argc, char **argv)
 {
    ros::init(argc, argv, "ros_test");
-   ros::NodeHandle n;
-   ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
-
    drawHouse(1);
 
    return 0;
